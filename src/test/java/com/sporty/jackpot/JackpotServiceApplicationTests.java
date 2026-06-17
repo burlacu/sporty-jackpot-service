171d2ef61c6815bdb29cefdb6a52dd0b5b0395dc
Demetrius19 <dumitru.burlacu@visma.com>
2026-06-17 16:04:54 +0300

    TS-1: project structure - initial Spring Boot project scaffold

---
+++ src/test/java/com/sporty/jackpot/JackpotServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sporty.jackpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackpotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
